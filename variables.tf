6558689eace7ea6b9a52e21b899df944ad01d72a
Jimmy <jimmy@acknowledge.fr>
2020-11-15 20:37:28 +0100

    clean(defaults): delete default variables

--- variables.tf
+++ variables.tf
@@ -15,19 +15,16 @@ variable "tenant_id" {
 variable "env" {
   description = "Environment name. Should be prod, dev or sandbox. Default to sandbox"
   type        = string
-  default     = "sandbox"
 }
 
 variable "project" {
   description = "Project name"
   type        = string
-  default     = "myapp"
 }
 
 variable "team" {
   description = "Feature team name"
   type        = string
-  default     = "team1"
 }
 
 #-------------------------#
@@ -37,13 +34,4 @@ variable "team" {
 variable networks {
   description = "A map containing virtual networks and associated subnets to create"
   type = map(any)
-  default = {
-    "vnet-1" : {
-      "address_space" : ["10.1.1.0/24"],
-      "subnets" : [
-        { "name" : "external", "address_prefix" : "10.1.1.0/28" },
-        { "name" : "internal", "address_prefix" : "10.1.1.16/28" }
-      ]
-    }
-  }
 }
