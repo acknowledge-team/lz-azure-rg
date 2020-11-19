8bc8430646e9a0d5e9ba93f3ac48dd5ab72cd8f4
Jims <jcasse@j-sec.fr>
2020-11-19 12:16:36 +0100

    feat(vnet): adding support to vnet as a list

--- networking/locals.tf
+++ networking/locals.tf
@@ -1,5 +1,5 @@
 locals {
   networks = {
-    for vnet in jsondecode(var.networks) : "vnet-${var.team}-${var.project}-${replace(vnet.vnet, "/", "-")}" => vnet
+    for vnet in jsondecode(var.networks) : "vnet-${var.team}-${var.project}-${vnet.id}" => vnet
   }
 }
