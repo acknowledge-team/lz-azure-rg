e910557a881f32e551dabef02f88672e76de39df
Jims <jcasse@j-sec.fr>
2020-11-19 17:53:40 +0100

    adding uuid in subnet and vnet names

--- networking/main.tf
+++ networking/main.tf
@@ -1,3 +1,7 @@
+resource "random_uuid" "uuid" {
+  for_each = local.networks
+}
+
 resource "azurerm_virtual_network" "virtual_network" {
   for_each            = local.networks
   name                = each.key
@@ -8,7 +12,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   dynamic "subnet" {
     for_each = toset(each.value.subnets)
     content {
-      name = "subnet-${var.team}-${var.project}-${replace(subnet.value, "/", "-")}"
+      name = "subnet-${var.team}-${var.project}-${replace(subnet.value, "/", "-")}-${random_uuid.uuid[each.key].id}"
       address_prefix = subnet.value
     }
   }
