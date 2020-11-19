5c2801cc294cc3a459c819a2386ee953d2ef99de
Jims <jcasse@j-sec.fr>
2020-11-19 18:06:50 +0100

    network id deleted in vnet name

--- networking/locals.tf
+++ networking/locals.tf
@@ -1,5 +1,5 @@
 locals {
   networks = {
-    for vnet in jsondecode(var.networks) : "vnet-${var.team}-${var.project}-${var.env}-${vnet.id}" => vnet
+    for vnet in jsondecode(var.networks) : "vnet-${var.team}-${var.project}-${var.env}" => vnet
   }
 }
--- networking/main.tf
+++ networking/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   dynamic "subnet" {
     for_each = toset(each.value.subnets)
     content {
-      name = "subnet-${var.team}-${var.project}-${replace(subnet.value, "/", "-")}-${random_uuid.uuid[each.key].result}"
+      name = "subnet-${var.team}-${var.project}-${replace(subnet.value, "/", "-")}"
       address_prefix = subnet.value
     }
   }
